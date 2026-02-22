60ec4d0f6275f5829d8b4ff87b765f12baa4e776
StedTechOrganization <stedtech.1209@gmail.com>
2026-02-22 17:33:10 +0900

    Update phone.js

--- js/phone.js
+++ js/phone.js
@@ -1,3 +1,24 @@
-document.getElementById("menu-toggle").addEventListener("click", function(){
-    document.querySelector("nav").classList.toggle("active");
+const nav = document.querySelector("nav");
+const toggle = document.getElementById("menu-toggle");
+
+/* ===== ハンバーガー開閉 ===== */
+toggle.addEventListener("click", function(e) {
+    e.stopPropagation(); // ←これ重要
+    nav.classList.toggle("active");
 });
+
+/* ===== メニュー外クリックで閉じる ===== */
+document.addEventListener("click", function(e) {
+
+    if (
+        nav.classList.contains("active") &&
+        !nav.contains(e.target) &&
+        !toggle.contains(e.target)
+    ) {
+        nav.classList.remove("active");
+    }
+});
+
+function close(){
+    nav.classList.remove("active");
+}
